bf98ec476188d33683ea2599876383f70323d40e
Paola Franco <paoelizabeth69@gmail.com>
2020-08-17 00:12:50 -0500

    Transaccion

---
+++ Transaccion/Data/MainContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Transaccion.Models;
+
+namespace Transaccion.Data
+{
+    public class MainContext : DbContext
+    {
+        public MainContext (DbContextOptions<MainContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Transaccion.Models.Cliente> Cliente { get; set; }
+    }
+}
